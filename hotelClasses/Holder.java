357424e7a4a448d01b07ca523584f942f21e9166
KevSky21 <105402578+KevSky21@users.noreply.github.com>
2025-07-01 17:46:58 -0500

    Create Holder.java

---
+++ hotelClasses/Holder.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.io.Serializable;
+
+public class Holder implements Serializable {
+    doubleroom[] luxury_doubleroom = new doubleroom[10];
+    doubleroom[] deluxe_doubleroom = new doubleroom[20];
+    singleroom[] luxury_singleroom = new singleroom[10];
+    singleroom[] deluxe_singleroom = new singleroom[20];
+}
