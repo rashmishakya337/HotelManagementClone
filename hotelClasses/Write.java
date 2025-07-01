4b8848074dd618330d6e6d63410152a389551fc6
KevSky21 <105402578+KevSky21@users.noreply.github.com>
2025-07-01 17:50:28 -0500

    Create Write.java

---
+++ hotelClasses/Write.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.io.*;
+
+public class Write implements Runnable {
+    Holder hotel_ob;
+
+    public Write(Holder hotel_ob) {
+        this.hotel_ob = hotel_ob;
+    }
+
+    @Override
+    public void run() {
+        try {
+            FileOutputStream fout = new FileOutputStream("backup");
+            ObjectOutputStream oos = new ObjectOutputStream(fout);
+            oos.writeObject(hotel_ob);
+        } catch (Exception e) {
+            System.out.println("Error in writing " + e);
+        }
+    }
+}
